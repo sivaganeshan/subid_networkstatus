fc3f37507fc0c276be4ac3807cd8c30a2ea7ecc3
sivaganeshan <bsivaganeshan@hotmail.com>
2022-01-05 11:35:36 +0530

    storyBook added

---
+++ src/stories/NetworkEleComp.stories.jsx
@@ -0,0 +1,12 @@
+import NetworkElement from "../components/NetworkElement";
+import NetworkEleWrapper from "../styles/NetworkEle.styles";
+
+export default{
+    title:"Components/NetworkElement",
+    component: NetworkElement,
+}
+
+ export const PossibleStates = ()=>(<><NetworkEleWrapper><NetworkElement /></NetworkEleWrapper></>);
+
+
+
